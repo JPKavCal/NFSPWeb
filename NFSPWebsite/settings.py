2d7f868c70542d428689260aa9140b1d9e6ab8c0
JP Calitz <205502158@stu.ukzn.ac.za>
2019-06-27 22:08:26 +0200

    GDal

--- NFSPWebsite/settings.py
+++ NFSPWebsite/settings.py
@@ -21,6 +21,7 @@ GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
