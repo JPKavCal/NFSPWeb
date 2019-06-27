2b6c97b96fefd1ccfa1504b00a081256609a79cc
JP Calitz <205502158@stu.ukzn.ac.za>
2019-06-27 21:19:41 +0200

    GDal

--- NFSPWebsite/settings.py
+++ NFSPWebsite/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
