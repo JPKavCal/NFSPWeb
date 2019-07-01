9cab9c8b89a46e20026885cb15eac4cb81af57db
JP Calitz <205502158@stu.ukzn.ac.za>
2019-07-01 22:43:15 +0200

    DFE implementation start

--- gaugedata/views.py
+++ gaugedata/views.py
@@ -12,18 +12,6 @@ def gauges(request):
 
 @login_required
 def dfe(request):
-    # return render(request, 'gaugedata/dfe.html')
+    gauges = RiverGauge.objects.all()
 
-    gauges = RiverGauge.objects.all()  # use filter() when you have sth to filter ;)
-    form = request.POST  # you seem to misinterpret the use of form from django and POST data. you should take a look
-                         # at [Django with forms][1]
-    # you can remove the preview assignment (form =request.POST)
-    # if request.method == 'POST':
-        # selected_item = get_object_or_404(Item, pk=request.POST.get('item_id'))
-        # get the user you want (connect for example) in the var "user"
-        # user.item = selected_item
-        # user.save()
-
-        # Then, do a redirect for example
-
-    return render_to_response('gaugedata/dfe.html', {'gauges': gauges})  #, context_instance=RequestContext(request), )
+    return render_to_response('gaugedata/dfe.html', {'gauges': gauges, 'user': request.user})
